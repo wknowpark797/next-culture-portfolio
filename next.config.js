5b3213bc01de2a49e222ef9d12da8f0be7ab84b6
unknown <wknowpark797@gmail.com>
2023-09-20 19:44:56 +0900

    next 필수 라이브러리 설치 및 next.config.js 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+	reactStrictMode: false,
+	sassOptions: {
+		includePath: [path.join(__dirname, 'styles')],
+		prependData: `@import "styles/_variables.scss";`,
+	},
+	compiler: { styledComponents: true },
+};
+
+module.exports = nextConfig;
