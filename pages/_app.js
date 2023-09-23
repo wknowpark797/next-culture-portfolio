34a1b13e11308c0bbeab7e9d2e426d28cb0c387c
unknown <wknowpark797@gmail.com>
2023-09-23 17:46:02 +0900

    서울시 문화행사 정보 오픈 API 데이터 호출

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import Layout from '@/components/template/Layout/Layout';
+import axios from 'axios';
 import '@/styles/globals.scss';
+
+axios.defaults.baseURL = 'http://openapi.seoul.go.kr:8088';
 
 export default function App({ Component, pageProps }) {
 	return (
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head';
+import axios from 'axios';
 
-export default function Home() {
+export default function Home({ data }) {
+	console.log(data.culturalEventInfo.row);
+
 	return (
 		<>
 			<Head>
@@ -10,4 +13,20 @@ export default function Home() {
 			<main>Main</main>
 		</>
 	);
+}
+
+// ISR 방식 (revalidate 주기설정)
+export async function getStaticProps() {
+	const KEY = process.env.CULTURE_PUBLIC_API_KEY;
+	const TYPE = 'json';
+	const SERVICE = 'culturalEventInfo';
+	const START_INDEX = 1;
+	const END_INDEX = 5;
+
+	const { data } = await axios.get(`${KEY}/${TYPE}/${SERVICE}/${START_INDEX}/${END_INDEX}/`);
+
+	return {
+		props: { data },
+		revalidate: 60 * 60 * 24,
+	};
 }
