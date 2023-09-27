1d4495c809edb76325306fca68f3c464bb43561b
unknown <wknowpark797@gmail.com>
2023-09-27 21:29:42 +0900

    Atoms 컴포넌트 스타일 적용 및 Component Description 페이지 생성

---
+++ pages/dev/index.jsx
@@ -0,0 +1,63 @@
+import Head from 'next/head';
+import { Btn } from '@/components/atoms/Button/Btn';
+import { Input } from '@/components/atoms/Form/Input';
+import { Table } from '@/components/atoms/Table/Table';
+import { useState } from 'react';
+import clsx from 'clsx';
+import styles from './style.module.scss';
+
+function index() {
+	const [InputValue, setInputValue] = useState('');
+
+	return (
+		<>
+			<Head>
+				<title>Components Description</title>
+			</Head>
+
+			<section className={clsx(styles.dev)}>
+				<h1>Components Description</h1>
+
+				<article>
+					<h2>Button Component</h2>
+					<Btn
+						onClick={() => {
+							alert('Click Event');
+						}}
+					>
+						Button Click
+					</Btn>
+					<Btn style={{ marginLeft: '10px' }} isActive={true}>
+						Button Active
+					</Btn>
+				</article>
+
+				<article>
+					<h2>Input Component</h2>
+					<Input value={InputValue} onChange={setInputValue} />
+					<p>value: {InputValue}</p>
+				</article>
+
+				<article>
+					<h2>Table Component</h2>
+					<Table
+						data={[
+							{
+								key1: 'value1-1',
+								key2: 'value1-2',
+								key3: 'value1-3',
+							},
+							{
+								key1: 'value2-1',
+								key2: 'value2-2',
+								key3: 'value2-3',
+							},
+						]}
+					/>
+				</article>
+			</section>
+		</>
+	);
+}
+
+export default index;
